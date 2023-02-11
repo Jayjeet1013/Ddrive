f810d7c3801f4fddeacca761240b45ca66cc4fff
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-11 20:57:03 +0530

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  },
 };
