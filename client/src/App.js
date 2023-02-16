3e1407ea3e9a955e886fdc25ff255df0b421cbc3
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-16 21:07:18 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+
 import  "./App.css"
 
 function  App(){
-  return <div className='App'>kya bolti hai company</div>
+  return <div className='App'>kya bolti hai company 
+  sabh thik hai na
+  </div>
 }
 
 export default App;
