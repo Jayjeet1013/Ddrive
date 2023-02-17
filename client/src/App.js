8f3dfce81e1f9fd002e66eb281f6796215ac39d2
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-17 21:53:12 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
-
+import Upload from './artifacts/contracts/Upload.sol/Upload.json'
 import  "./App.css"
-
+import {useState, useEffect} from 'react'
+import {ethers} from 'ethers'
+import FileUpload from './components/FileUpload'
+import Modal from './components/Modal'
+import Display from './components/Display'
 function  App(){
   return <div className='App'>kya bolti hai sabh thik hai na
   </div>
