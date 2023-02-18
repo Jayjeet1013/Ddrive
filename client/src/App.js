e27f01937ed47facf324c9922f19825b96e75423
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-18 11:23:11 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,33 @@ import FileUpload from './components/FileUpload'
 import Modal from './components/Modal'
 import Display from './components/Display'
 function  App(){
-  return <div className='App'>kya bolti hai sabh thik hai na
-  </div>
+  const [account,setAccount] =useState("");
+  const [contract,setContract] =useState(null);
+  const [provider,setProvider] = useState(null);
+  const [modalOpen,setModalOpen]= useState(false);
+  
+  useEffect(()=>{
+    const provider= new ethers.providers.Web3Provider(window.ethereum);
+    const loadProvider=async()=>{
+      if(provider){
+        await provider.send("eth_requestAccounts",[]);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        let contractAddress = " 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+        const contract = new ethers.Contract(
+          contractAddress,Upload.abi,signer
+        )
+          setContract(contract)
+          setProvider(provider)
+      }else{
+        console.log("metamask is not installed");
+      }
+    };
+    provider && loadProvider();
+  },[]);
+    return <div className='App'> </div>
 }
 
 export default App;
