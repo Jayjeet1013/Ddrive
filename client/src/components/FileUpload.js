f997c5b8a70772b30b587ad2f12594fdd20c7e3b
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-24 21:43:38 +0530

    update

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -2,8 +2,13 @@ import { useState } from "react"
 import axios from 'axios'
 import './FileUpload.css'
 const FileUpload=({contract, account, provider})=> {
+  const [file,setFile]=useState(null);
+  const [fileName,setFileName]=useState("No image selected");
   const handleSubmit=async(e)=>{
-
+  e.preventDefault();
+  if(file){
+    try
+  }
   }
   const retrieveFile=()=>{
 
@@ -14,7 +19,7 @@ const FileUpload=({contract, account, provider})=> {
         choose Image
       </label>
       <input disabled={!account} type="file" id="file-upload" name="data" onChange={retrieveFile}/>
-    <span className="textArea">Image:#temporary.png</span>
+    <span className="textArea">Image: {fileName}</span>
     <button type="submit" className="upload">upload File</button>
     </form>
   </div>
