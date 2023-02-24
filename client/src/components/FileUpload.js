439466593054eebedbc46b5a18ab571f1422b678
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-24 14:13:12 +0530

    update

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -2,8 +2,21 @@ import { useState } from "react"
 import axios from 'axios'
 import './FileUpload.css'
 const FileUpload=({contract, account, provider})=> {
+  const handleSubmit=async(e)=>{
+
+  }
+  const retrieveFile=()=>{
+
+  }
   return <div className="top">
-    <form className=" form" onSubmit={handleSubmit}></form>
+    <form className=" form" onSubmit={handleSubmit}>
+      <label htmlFor="file-upload" className="choose">
+        choose Image
+      </label>
+      <input disabled={!account} type="file" id="file-upload" name="data" onChange={retrieveFile}/>
+    <span className="textArea">Image:#temporary.png</span>
+    <button type="submit" className="upload"></button>
+    </form>
   </div>
 }
 
