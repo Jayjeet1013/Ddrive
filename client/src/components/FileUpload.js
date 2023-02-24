e7e6dee666257733a0febe00f0522865f9e8efaa
Jayjeet1013 <jayjeet1013@gmail.com>
2023-02-24 13:55:39 +0530

    update

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import axios from 'axios'
-import './FileUpload.css  '
+import './FileUpload.css'
 const FileUpload=({contract, account, provider})=> {
-  return <></>
+  return <div className="top">
+    <form className=" form" onSubmit={handleSubmit}></form>
+  </div>
 }
 
-export default FileUpload
+export default FileUpload;
